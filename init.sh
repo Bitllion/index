461a54fba40d9eba1244ccb867d1a6315afbd468
fanyueqiang <fanyq.mr@gmail.com>
2022-06-27 19:09:25 +0800

    :smile:

---
+++ init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+
+yum install -y yum-utils
+
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+systemctl enable docker --now
+
+yum install 
