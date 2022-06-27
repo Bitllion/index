f49b31f422cab51770a0235c60c4960643eb5ef1
fanyueqiang <fanyq.mr@gmail.com>
2022-06-27 19:35:40 +0800

    :smile:

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
 #1.指定基础镜像，并且必须是第一条指令
 FROM httpd:alpine
 
-#2.指明该镜像的作者和其电子邮件
-LABEL maintainer="Bitllion"
+#2.指明该镜像的作者
+LABEL maintainer="bitllion"
 
 #3.在构建镜像时，指定镜像的工作目录，之后的命令都是基于此工作目录，如果不存在，则会创建目录
 WORKDIR /usr/local/apache2/
 
 #4.下载压缩包
-RUN wget https://mirror.ghproxy.com/https://github.com/Bitllion/index/archive/refs/tags/v1.0.0.zip
+RUN wget https://gh.ddlc.top/https://github.com/Bitllion/index/archive/refs/tags/v1.0.0.zip
 RUN unzip v1.0.0.zip
 RUN mv index-1.0.0/* htdocs/
 
--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+yum update -y
 yum remove docker \
                   docker-client \
                   docker-client-latest \
@@ -15,8 +16,16 @@ yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 
-yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+yum install -y docker-ce docker-ce-cli containerd.io    
 
+wget https://mirror.ghproxy.com/https://github.com/docker/compose/releases/download/v2.6.1/docker-compose-linux-x86_64
+chmod +x docker-compose-linux-x86_64
+mv docker-compose-linux-x86_64 /usr/bin/docker-compose
 systemctl enable docker --now
 
-yum install 
+mkdir index
+cd index
+wget https://ghproxy.futils.com/https://github.com/Bitllion/index/blob/main/Dockerfile
+# docker build -t index .
+wget https://ghproxy.futils.com/https://github.com/Bitllion/index/blob/main/docker-compose.yml
+docker-compose up -d
